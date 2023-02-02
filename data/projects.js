931c7ef60ccf1fb65a025b9564444775718daa65
Juan Martín García <hello@juangarcia.com.ar>
2023-02-02 16:58:17 -0300

    Use projects data in homepage

---
+++ data/projects.js
@@ -0,0 +1,80 @@
+const projects = [
+  {
+    "shortTitle": "estomba",
+    "longTitle": "estomba 1046",
+    "location": "villa ortuzar, ciudad de buenos aires",
+    "role": "proyecto y dirección de obra",
+    "year": "2022 – 2025",
+    "thumbnail": "estomba",
+    "order": 1
+  },
+  {
+    "shortTitle": "galicia",
+    "longTitle": "galicia 2328",
+    "location": "paternal, ciudad de buenos aires",
+    "role": "anteproyecto y cotización preliminar.",
+    "status": "en presentación municipal.",
+    "year": "2020 – 2023",
+    "thumbnail": "galicia",
+    "order": 2
+  },
+  {
+    "shortTitle": "comodoro rivadavia",
+    "longTitle": "comodoro rivadavia 1752",
+    "location": "nuñez, ciudad de buenos aires",
+    "role": "proyecto y dirección de obra.",
+    "status": "en cotización construcción.",
+    "year": "2020 – 2021",
+    "thumbnail": "comodoro-rivadavia",
+    "order": 3
+  },
+  {
+    "shortTitle": "heredia",
+    "longTitle": "heredia 986",
+    "location": "villa ortuzar, ciudad de buenos aires.",
+    "role": "anteproyecto y cotización preliminar.",
+    "year": "2019",
+    "thumbnail": "heredia",
+    "order": 4
+  },
+  {
+    "shortTitle": "teodoro garcía",
+    "longTitle": "teodoro garcía 3724",
+    "location": "chacarita, ciudad de buenos aires.",
+    "role": "proyecto y dirección de obra.",
+    "status": "en obra.",
+    "year": "2018 – 2022",
+    "thumbnail": "teodoro-garcia",
+    "order": 5
+  },
+  {
+    "shortTitle": "olaguer y feliú",
+    "longTitle": "olaguer y feliú 2855",
+    "location": "colegiales, ciudad de buenos aires.",
+    "role": "proyecto y dirección de obra.",
+    "status": "en obra.",
+    "year": "2019 – 2022",
+    "thumbnail": "olaguer-y-feliu",
+    "order": 6
+  },
+  {
+    "shortTitle": "yaciretá",
+    "longTitle": "pabellón yaciretá",
+    "location": "pdo. san martín, provincia de bs. as.",
+    "role": "proyecto y dirección de obra.",
+    "status": "construido.",
+    "year": "2012",
+    "thumbnail": "yacireta",
+    "order": 7
+  },
+  {
+    "shortTitle": "oficinas pq",
+    "longTitle": "oficinas pq - libero",
+    "location": "palermo, ciudad de buenos aires.",
+    "role": "proyecto y dirección de obra.",
+    "status": "construido.",
+    "year": "2020",
+    "thumbnail": "oficinas-pq",
+    "order": 8
+  },
+];
--- src/components/layout/projects/more-projects.js
+++ src/components/layout/projects/more-projects.js
@@ -1,9 +1,20 @@
 class MoreProjects extends HTMLElement {
   connectedCallback() {
-    const { image, extension, title, location, role, year } = this.dataset;
+    const {  } = this.dataset;
 
     this.innerHTML = `
-      <p>test</p>
+      <div class="container mx-auto flex flex-col md:flex-row items-end justify-between px-4">
+        <h4 class="font-sans text-primary text-2xl md:text-3xl">more projects</h4>
+        <!-- Arrow -->
+        <svg width="64" height="64" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg"
+          class="ml-auto fill-primary mb-2">
+          <path
+            d="M63.4867 61.8074L64 0C48.519 0.150494 17.6712 0.362806 2.1916 0.511958L2.06395 8.24889C15.9887 8.11183 34.4745 7.99896 50.7388 7.85117L0 58.5891L5.41099 64L56.1497 13.2621L55.709 61.8948L63.4891 61.8075L63.4867 61.8074Z" />
+        </svg>
+      </div>
+      <ul>
+        
+      </ul>
     `;
   }
 };
