dace6d697c3b39d89cd4420e77a50283aeb2062c
Juan Martín García <hello@juangarcia.com.ar>
2023-07-14 12:19:44 -0300

    Fix footer company

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ const Footer = ({ classes }) => {
   return (
     <footer className={`flex flex-col lg:flex-row items-baseline justify-between pb-4 md:pb-8 ${classes ? classes : ''}`}>
       <p className="font-sans uppercase text-xs text-gray-900 tracking-widest">
-        Copyright &copy; {new Date().getFullYear()} BA AA.
+        Copyright &copy; {new Date().getFullYear()} BAS.
       </p>
       <p className="font-sans uppercase text-xs text-gray-900 tracking-widest mt-1 md:mt-0">
         Website by {" "}
