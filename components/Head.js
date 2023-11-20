edc0efc42d55dc57b13e3dd0bbb683b8b85ea792
Juan Martín García <hi@juanmartingarcia.com>
2023-11-20 12:02:47 -0300

    Update Head.js

--- components/Head.js
+++ components/Head.js
@@ -8,7 +8,8 @@ const CustomHead = ({ pageTitle }) => {
       <meta charSet="UTF-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      
+      <meta name="description" content="Estudio de arquitectura con base en la Ciudad de Buenos Aires que trabaja en encargos de servicios profesionales en Argentina, Estados Unidos y Europa, y concursos nacionales e internacionales." />
+    
       <title>{title}</title>
       
       <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
